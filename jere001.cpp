ef6b53821cfb97c1513c38d925246ea7011fd3e6
jeremymokua <113586412+jeremymokua@users.noreply.github.com>
2022-10-06 13:39:28 +0300

    Add files via upload
    do while program

---
+++ jere001.cpp
@@ -0,0 +1,29 @@
+// program using do..while loop to display numbers
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+ //initialising the variable
+  
+  int i=10;
+   
+ do
+   
+ {
+cout<<i<<"";
+ 
+   i--;
+}
+//condition;
+
+while (i>=1);
+
+//program terminates when the condition becomes false
+
+return 0;
+
+}
